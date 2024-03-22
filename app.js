51c20fdcbe7f14b82e9dc6f280807aa79586288e
0SCARMB <152223836+0SCARMB@users.noreply.github.com>
2024-03-22 10:11:12 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+//base de datos local de preguntas
+const bd_juego = [
+    {
+        id:0,
+        pregunta:"Cal destes autores non pertence á Xeración do 27?",
+        op0:"Federico García Lorca",
+        op1:"Luis Cernuda",
+        op2:"Pedro Salinas",
+        op3:"Valle-Inclán",
+        correcta:"2"
+    },
+    {
+        id:1,
+        pregunta:" ",
+        op0:"Floridablanca",
+        op1:"Francisco José I",
+        op2:"Fernando VII",
+        op3:"Esquilache",
+        correcta:"2"
+    },
+]
