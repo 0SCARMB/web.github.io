73e2ffa78b987465d3922a7e2188890fac080290
0SCARMB <152223836+0SCARMB@users.noreply.github.com>
2024-04-19 10:21:02 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -7,15 +7,89 @@ const bd_juego = [
         op1:"Luis Cernuda",
         op2:"Pedro Salinas",
         op3:"Valle-Inclán",
-        correcta:"2"
+        correcta:"3"
     },
     {
         id:1,
-        pregunta:" ",
-        op0:"Floridablanca",
-        op1:"Francisco José I",
-        op2:"Fernando VII",
-        op3:"Esquilache",
+        pregunta:"Cal destes autores non pertence á Xeración do 98? ",
+        op0:"Pío Baroja",
+        op1:"Miguel de Unamuno",
+        op2:"Rubén Darío",
+        op3:"José Martínez Ruiz(Azorín)",
         correcta:"2"
     },
+    {
+        id:2,
+        pregunta:"Cal destes movementos artísticos e literarios non pertencen ás vanguardias?",
+        op0:"Renacimiento",
+        op1:"Futurismo",
+        op2:"Cubismo",
+        op3:"Surrealismo",
+        correcta:"0"
+    },
+    {
+        id:3,
+        pregunta:"Cal destes autores non destacou durante o 'Siglo de Oro'?",
+        op0:"Miguel de Cervantes",
+        op1:"Francisco Gómez de Quevedo",
+        op2:"Lope de Vega",
+        op3:"Gustavo Adolfo Bécquer",
+        correcta:"3"
+    },
+    {
+        id:4,
+        pregunta:"Cal destes autores non pertence á literatura española?",
+        op0:"Antonio Machado",
+        op1:"Lope de Vega",
+        op2:"William Shakespeare",
+        op3:"Benito Pérez Galdós",
+        correcta:"2"
+    },
+    {
+        id:5,
+        pregunta:"Cal destes autores escribiu 'Don Quijote de la Mancha'?",
+        op0:"Federico García Lorca",
+        op1:"Miguel de Cervantes",
+        op2:"Pedro Salinas",
+        op3:"Valle-Inclán",
+        correcta:"1"
+    },
+    {
+        id:6,
+        pregunta:"Cal destes autores pertence á Literatura Castellana?",
+        op0:"Eduardo Blanco Amor",
+        op1:"Alfonso Daniel Rodríguez Castelao",
+        op2:"Rafael Alberti",
+        op3:"Álvaro Cunqueiro",
+        correcta:"2"
+    },
+    {
+        id:7,
+        pregunta:"Cal destes autores escribiu 'Don Juan Tenorio'?",
+        op0:"José Zorrilla",
+        op1:"Gustavo Adolfo Bécquer",
+        op2:"Antonio Machado",
+        op3:"Ramón María del Valle Inclán",
+        correcta:"0"
+    },
+    {
+        id:8,
+        pregunta:"Quen escribiu 'Lazarillo de Tormes'?",
+        op0:"Lope de Vega",
+        op1:"Miguel de Cervantes",
+        op2:"Autor Anónimo",
+        op3:"Emilia Pardo Bazán",
+        correcta:"2"
+    },
+    {
+        id:9,
+        pregunta:"Cal destes autores escribiu 'La Regenta'?",
+        op0:"Almudena Grandes",
+        op1:"Antonio Machado",
+        op2:"Benito Pérez Galdós",
+        op3:"Leopoldo Alas, Clarín",
+        correcta:"3"
+    }
 ]
+
+
